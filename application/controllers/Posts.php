16f9d533375e8e27a863327d3aaa04b7213023bc
Basil Al-Tamiim <basil.altamimi@intigral.net>
2018-01-19 15:40:44 +0400

    Add "Display Comments" section

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -14,6 +14,10 @@ class Posts extends CI_Controller {
 	public function view($slug = NULL){
 		$data['post'] = $this->post_model->get_posts($slug);
 
+		// Fetch post's comments
+		$post_id = $data['post']['id'];
+		$data['comments'] = $this->comment_model->get_comments($post_id);
+
 		if(empty($data['post'])){
 			show_404();
 		}
