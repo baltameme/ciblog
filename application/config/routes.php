799b9f3a50a088d6491d90506d45362e9ebf7293
Basil Al-Tamiim <basil.altamimi@intigral.net>
2018-01-19 13:18:42 +0400

    Add Categories section

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['categories'] = 'categories/index';
+$route['categories/create'] = 'categories/create';
+$route['categories/posts/create'] = 'categories/posts/$1';
+
 $route['posts/create'] = 'posts/create';
 $route['posts/update'] = 'posts/update';
 $route['posts/(:any)'] = 'posts/view/$1';
